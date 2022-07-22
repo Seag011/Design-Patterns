2cd2ee7a0194f72d89c03459a74f6a7346ac5043
Павел <pavlik3312@gmail.com>
2022-07-22 22:15:56 +0300

    added facade

---
+++ c++/c++/Structural/facade.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+
+namespace Facede {
+
+	class Facade {
+	public:
+		Facade() {
+			a = &ComponentA();
+			b = &ComponentB();
+			c = &ComponentC();
+
+		}
+		void HelloWorld() {
+			a->DoA();
+			b->DoB();
+			c->DoC();
+		}
+	private:
+		ComponentA* a;
+		ComponentB* b;
+		ComponentC* c;
+	};
+
+
+	class ComponentA{
+	public:
+		void DoA() {
+			std::cout << "Hello";
+		}
+	};
+
+	class ComponentB {
+	public:
+		void DoB() {
+			std::cout << " wo";
+		}
+	};
+
+	class ComponentC {
+	public:
+		void DoC() {
+			std::cout << "rld!" << std::endl;
+		}
+	};
+}
